b6927f9797272b5899b1c60c387b2deff8a6de41
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-08 22:30:21 -0200

    adding typescript

--- src/index.js
+++
@@ -1,1 +0,0 @@
-console.log('this is just a basic project structure for JavaScript development');
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function howdy(name: string): string {
+  return `Hi, ${name}`;
+}
+
+console.log(howdy('you'));
