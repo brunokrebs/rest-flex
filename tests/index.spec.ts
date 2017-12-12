4be3db7775b39d7e7ff93ef56ebf3a42e5fe601d
Bruno Krebs <bruno.krebs@auth0.com>
2017-12-12 00:33:11 -0200

    reorganizing tests and trying to reach 100% on coverage

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,32 +1,42 @@
 import './mocks';
 import * as chai from 'chai';
 import * as restFlex from  '../src/index';
+import * as server from '../src/server';
 import * as request from 'request-promise';
 
+
 describe('tests all the way', () => {
-  it('should say hi to Bruno', async () => {
-    const options = {
-      uri: 'http://localhost:3001/',
-      headers: {
-        'Authorization': 'Bearer some.access.token'
-      },
-      json: true
-    };
+  it('should say hi to Bruno', sayHi);
+  it('should be able to handle exceptions', handleErrors);
+  it('should not generate errors when shutingdown without starting', checkStoppedShutdown);
 
-    const result = await request(options);
-    chai.assert(result === 'Hi!', 'It should say "Hi!"');
-  });
-
-  it('should be able to handle exceptions', async () => {
-    try {
-      await request('http://localhost:3001/forge-error');
-    } catch (err) {
-      chai.assert(err.statusCode === 400);
-      chai.assert(JSON.parse(err.response.body).message === 'Oops! Something went wrong.');
-    }
-  });
-
-  after(async () => {
-    await restFlex.stop();
+  after(() => {
+    restFlex.stop();
   })
 });
+
+async function sayHi() {
+  const options = {
+    uri: 'http://localhost:3001/',
+    headers: {
+      'Authorization': 'Bearer some.access.token'
+    },
+    json: true
+  };
+
+  const result = await request(options);
+  chai.assert(result === 'Hi!', 'It should say "Hi!"');
+}
+
+async function handleErrors() {
+  try {
+    await request('http://localhost:3001/forge-error');
+  } catch (err) {
+    chai.assert(err.statusCode === 400);
+    chai.assert(JSON.parse(err.response.body).message === 'Oops! Something went wrong.');
+  }
+}
+
+async function checkStoppedShutdown() {
+  server.shutdown();
+}
